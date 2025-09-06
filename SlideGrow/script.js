89ec0d6e13c23dfb8d3ea0437c6e19901343dca3
sujan-roy24 <sjnry25@gmail.com>
2025-09-07 01:31:48 +0600

    Slide Grow  project with HTML, CSS and JS

---
+++ SlideGrow/script.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        cards.forEach(c => c.classList.remove('active'));
+        card.classList.add('active');
+    });
+});
